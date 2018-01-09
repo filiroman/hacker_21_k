418dfc4e7f896a35824d418cbac4d69a2175b10e
Roman Filippov <filiroman.tsu@gmail.com>
2018-01-10 02:10:23 +0300

    Initial Commit

---
+++ Hacker 21K/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hacker 21K
+//
+//  Created by Roman Filippov on 10/01/2018.
+//  Copyright © 2018 romanfilippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
